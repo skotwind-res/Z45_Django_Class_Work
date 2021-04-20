20dab5e8a32158762343c62d727e2d1c168b4261
skotwind-res <skotwind.res.2@gmail.com>
2021-04-20 21:59:15 +0300

    Class work. Add more all.

--- samplesite/bboard/views.py
+++ samplesite/bboard/views.py
@@ -1,13 +1,17 @@
-from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import render
+
 from datetime import datetime
 
-
-# Create your views here.
+from .models import Bb
 
 def index(request):
-    return HttpResponse(f"Здесь будет выведен список объявлений.")
+  bbs = Bb.objects.all()
+  return render(request, 'bboard/index.html', {'bbs': bbs})
 
 def cur_time(request):
     return HttpResponse(f"Current time: {datetime.now().hour}:{datetime.now().minute}")
+
+
+
 
