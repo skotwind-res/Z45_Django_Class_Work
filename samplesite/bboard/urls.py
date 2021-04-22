e018467ff951b8017ca86c7192d54d9dde56001b
skotwind-res <skotwind.res.2@gmail.com>
2021-04-22 20:08:14 +0200

    Add function for add.

--- samplesite/bboard/urls.py
+++ samplesite/bboard/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import index, cur_time, by_rubric
+from .views import index, cur_time, by_rubric, BbCreateView
 
 urlpatterns = [
-    path('', index),
-    path('<int:rubric_id>/', by_rubric),
+    path('<int:rubric_id>/', by_rubric, name='by_rubric'),
+    path('', index, name='index'),
+    path('add/', BbCreateView.as_view(), name='add'),
     path('cur_time/', cur_time),
 ]
 
--- samplesite/bboard/views.py
+++ samplesite/bboard/views.py
@@ -1,10 +1,23 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-from .models import Rubric
+from .models import Rubric, Bb
+from django.views.generic.edit import CreateView
+from django.urls import reverse_lazy
+
+from .forms import BbForm
 
 from datetime import datetime
 
-from .models import Bb
+
+class BbCreateView(CreateView):
+    template_name = 'bboard/create.html'
+    form_class = BbForm
+    success_url = reverse_lazy('index')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['rubrics'] = Rubric.objects.all()
+        return context
 
 
 def index(request):
@@ -24,4 +37,7 @@ def by_rubric(request, rubric_id):
     current_rubric = Rubric.objects.get(pk=rubric_id)
     context = {'bbs': bbs, 'rubrics': rubrics, 'current_rubric': current_rubric}
     return render(request, 'bboard/by_rubric.html', context)
+
+
+
 
