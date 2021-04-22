73889caaef74d3189fdfcfe7c3ec4c5a81d9cc7d
skotwind-res <skotwind.res.2@gmail.com>
2021-04-22 19:24:46 +0200

    Add rubrics and new views.

--- samplesite/bboard/urls.py
+++ samplesite/bboard/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import index, cur_time
+from .views import index, cur_time, by_rubric
 
 urlpatterns = [
     path('', index),
+    path('<int:rubric_id>/', by_rubric),
     path('cur_time/', cur_time),
 ]
 
--- samplesite/bboard/views.py
+++ samplesite/bboard/views.py
@@ -1,17 +1,27 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Rubric
 
 from datetime import datetime
 
 from .models import Bb
 
+
 def index(request):
-  bbs = Bb.objects.all()
-  return render(request, 'bboard/index.html', {'bbs': bbs})
+    bbs = Bb.objects.all()
+    rubrics = Rubric.objects.all()
+    context = {'bbs': bbs, 'rubrics': rubrics}
+    return render(request, 'bboard/index.html', context)
+
 
 def cur_time(request):
     return HttpResponse(f"Current time: {datetime.now().hour}:{datetime.now().minute}")
 
 
-
+def by_rubric(request, rubric_id):
+    bbs = Bb.objects.filter(rubric=rubric_id)
+    rubrics = Rubric.objects.all()
+    current_rubric = Rubric.objects.get(pk=rubric_id)
+    context = {'bbs': bbs, 'rubrics': rubrics, 'current_rubric': current_rubric}
+    return render(request, 'bboard/by_rubric.html', context)
 
